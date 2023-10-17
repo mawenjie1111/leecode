f1f17edb6bcdcaae6bb50856515de6cf0183dee3
mawenjie1111 <1468382678@qq.com>
2023-10-17 11:48:45 +0800

    LeetCode169

--- LeetCode169.cpp
+++ LeetCode169.cpp
@@ -3,41 +3,46 @@
 #include <unordered_map>
 using namespace std;
 
-int majorityElement(vector<int>& nums) {
-    unordered_map<int, int> hashtable; 
-    int len=nums.size();
+int majorityElement(vector<int> &nums)
+{
+    unordered_map<int, int> hashtable;
+    int len = nums.size();
     for (int i = 0; i < nums.size(); i++)
     {
         hashtable[nums[i]]++;
-        if (hashtable[nums[i]] >len / 2)
+        if (hashtable[nums[i]] > len / 2)
         {
             return nums[i];
         }
     }
     return 0;
-    }
+}
 
-int majorityElement_better(vector<int>& nums) {
-    //Boyer-Moore Voting Algorithm
-    //time complexity:O(n)
-    //space complexity:O(1)
-    int count=0;
-    int candidate=0;
-    int len=nums.size();
-    for(int i=0;i<len;i++){
-        if(count==0){
-            candidate=nums[i];
+int majorityElement_better(vector<int> &nums)
+{
+    // Boyer-Moore Voting Algorithm
+    // time complexity:O(n)
+    // space complexity:O(1)
+    int count = 0;
+    int candidate = 0;
+    int len = nums.size();
+    for (int i = 0; i < len; i++)
+    {
+        if (count == 0)
+        {
+            candidate = nums[i];
         }
-        if(nums[i]==candidate){
+        if (nums[i] == candidate)
+        {
             count++;
         }
-        else{
+        else
+        {
             count--;
         }
     }
     return candidate;
-    }
-
+}
 
 int main(int argc, char const *argv[])
 {
