01beacf791d70f2147b738f38e27d61712535930
mawenjie1111 <1468382678@qq.com>
2023-10-24 14:53:11 +0800

    LeetCode274

--- LeetCode274.cpp
+++ LeetCode274.cpp
@@ -1,56 +1,24 @@
 #include <iostream>
 #include <vector>
-#include <cmath>
+#include <algorithm>
 using namespace std;
-
-vector<int> quickSort(vector<int>& citations)
-{
-    int len=citations.size();
-    if(len<=1)
-    {
-        return citations;
-    }
-    int pivot=citations[0];
-    vector<int> left;
-    vector<int> right;
-    for(int i=1;i<len;i++)
-    {
-        if(citations[i]>=pivot)
-        {
-            left.push_back(citations[i]);
-        }
-        else
-        {
-            right.push_back(citations[i]);
-        }
-    }
-    left=quickSort(left);
-    right=quickSort(right);
-    left.push_back(pivot);
-    for(int i=0;i<right.size();i++)
-    {
-        left.push_back(right[i]);
-    }
-    return left;
-}
-
 int hIndex(vector<int>& citations) {
-    citations=quickSort(citations);
-    cout<<citations[0]<<endl;
+    sort(citations.begin(), citations.end(),greater<int>());
+    if(citations[0]==0)
+    {
+        return 0;
+    }
     int len=citations.size();
+    int result=len;
     for (int i = 0; i < len; i++)
     {
-        if (i+1==citations[i])
+        if (i+1>citations[i])
         {
-            return i+1;
+            result=i;
+            break;
         }
         /* code */
     }
-    return 1;
+    return result;
     }
-int main(int argc, char const *argv[])
-{
-    vector<int> citations={3,0,6,1,5};
-    hIndex(citations);
-    return 0;
-}
+
